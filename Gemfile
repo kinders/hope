2515550e0ab9f4456b41938b987d8243ab7c2dc0
kinders <zuihuizhaomafan@163.com>
2019-06-13 19:37:10 +0800

    0.5.10 upgrade 5.2.3

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
-source 'https://ruby.taobao.org'
+source 'https://gems.ruby-china.com'
 
 git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
@@ -7,7 +7,7 @@ end
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.0.1'
+gem 'rails', '~> 5.2.0'
 # Use sqlite3 as the database for Active Record
 #gem 'sqlite3'
 # Use Puma as the app server
@@ -40,6 +40,7 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
-gem 'wx_pay', git: 'https://github.com/kinders/wx_pay.git', branch: 'kinders-patch-1'
+gem 'wx_pay'
 gem 'paranoia'
 gem 'pg'
+gem 'bootsnap'
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
-    'Cache-Control' => 'public, max-age=3600'
+    'Cache-Control' => "public, max-age=#{1.hour.to_i}"
   }
 
   # Show full error reports and disable caching.
@@ -26,13 +26,10 @@ Rails.application.configure do
   config.action_dispatch.show_exceptions = false
 
   # Disable request forgery protection in test environment.
-#  config.action_controller.allow_forgery_protection = false
-#  config.action_mailer.perform_caching = false
+  config.action_controller.allow_forgery_protection = false
 
-  # Tell Action Mailer not to deliver emails to the real world.
-  # The :test delivery method accumulates sent emails in the
-  # ActionMailer::Base.deliveries array.
-#  config.action_mailer.delivery_method = :test
+  # Store uploaded files on the local file system in a temporary directory
+  # config.active_storage.service = :test
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
